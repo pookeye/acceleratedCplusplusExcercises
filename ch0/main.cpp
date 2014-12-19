40b56ac286513cd2135e52d48640b93aab6dc5b7
pookeye <silentterms@gmail.com>
2014-12-18 18:59:06 -0600

    Create main.cpp

---
+++ ch0/main.cpp
@@ -0,0 +1,28 @@
+//
+//  excercixe04.cpp
+//  HelloWorld
+//
+//  Created by Daniel Chan on 12/13/14.
+//  Copyright (c) 2014 Pookeye Inc. All rights reserved.
+//
+
+#include <iostream>
+
+int main() {
+    // excercixe04.cpp
+    {
+       //test test // test test //test test
+        // test // testett /* test
+        // or */ to delimit 
+    std::cout << "// a small C++ program" << std::endl;
+    std::cout << "#include <iostream>" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "int main()" << std::endl;
+    std::cout << "{" << std::endl;
+    std::cout << "\t std::cout << \"Hello, World!\" << std:endl;" << std::endl;
+    std::cout << "\t return 0;" << std::endl;
+    std::cout << "}" << std::endl;
+    }
+    
+    return 0;
+}
