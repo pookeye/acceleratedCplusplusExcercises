66da29e6d066f527ab5108ad0934afd5c44dbd92
pookeye <silentterms@gmail.com>
2014-12-20 22:41:26 -0600

    Create main.cpp

---
+++ ch1/excecise/1-3/main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  Chapter1Excercises
+//
+//  Created by Daniel Chan on 12/20/14.
+//  Copyright (c) 2014 Pookeye Inc. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+
+int main() {
+    // insert code here...
+    { const std::string s = "a string";
+        std::cout << s << std::endl;}
+    { const std::string s = "another string";
+        std::cout << s << std::endl; }
+    
+    return 0;
+}
